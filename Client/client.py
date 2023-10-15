e80c510b50a757e7a252daa58b89f540735ea560
Nikita Igumnov <nkigumnov@yandex.ru>
2023-10-15 10:12:08 +0300

    Fix codestyle

--- Client/client.py
+++ Client/client.py
@@ -1,38 +1,41 @@
-import PySimpleGUI as sg
 import socket
 import sys
 
-sg.theme('Light Blue 2')
+import PySimpleGUI as sg
 
-layout = [[sg.Text('Load Documents')],
-          [sg.Text('File', size=(8, 1)), sg.Input(key='Input'), sg.FileBrowse()],
-          [sg.Submit(), sg.Exit()]]
+sg.theme("Light Blue 2")
 
-window = sg.Window('Check Document', layout)
+layout = [
+    [sg.Text("Load Documents")],
+    [sg.Text("File", size=(8, 1)), sg.Input(key="Input"), sg.FileBrowse()],
+    [sg.Submit(), sg.Exit()]
+]
+
+window = sg.Window("Check Document", layout)
 
 count = 0
 while True:
     if count > 1:
         break
     event, values = window.read()
-    file = values['Input']
-    if event == 'Exit' and file == "":
+    file = values["Input"]
+    if event == "Exit" and file == "":
         break
     elif event in (sg.WINDOW_CLOSED, "Exit"):
         sys.exit()
-    elif event == 'Submit':
-        if '' != file:
+    elif event == "Submit":
+        if "" != file:
             count += 1
             s = socket.socket()
-            s.connect(("127.0.0.3",9999))
-            f = open (file, "rb")
+            s.connect(("127.0.0.3", 9999))
+            f = open(file, "rb")
             l = f.read(1024)
-            while (l):
+            while l:
                 s.send(l)
                 l = f.read(1024)
             s.close()
 
-if values['Input'] == "":
+if values["Input"] == "":
     sys.exit()
 
 window.close()
--- Server/server.py
+++ Server/server.py
@@ -1,24 +1,24 @@
 import socket
 import sys
+
+
 s = socket.socket()
-s.bind(("127.0.0.3",9999))
+s.bind(("127.0.0.3", 9999))
 s.listen(10)
 
-i = 1
-
-count = 0
+i, count = 1, 0
 while True:
     sc, address = s.accept()
 
-    f = open('./upload/file_' + str(i) + ".pdf",'wb')
-    i = i + 1
+    f = open(f"./upload/file_{i}.pdf", "wb")
+    i += 1
     l = sc.recv(1024)
     new_try = True
-    while (l): 
+    while l:
         if new_try:
             count += 1
             new_try = False
-            
+
         f.write(l)
         l = sc.recv(1024)
     f.close()
